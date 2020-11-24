1b07db4454f4c629b4f8d1f79fe6a50f59a3127d
CocotierMon <mmjanczak@gmail.com>
2020-11-24 11:38:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  //const apiRoot = 'https://gentle-shelf-85334.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'https://gentle-shelf-85334.herokuapp.com/v1/task/';
+  //const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
